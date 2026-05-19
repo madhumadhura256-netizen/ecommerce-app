540800cc87375f62d138ed2e6c18b1752e257e14
madhu <madhu>
2026-05-19 14:49:38 +0530

    backend fixing

--- backend/server.js
+++ backend/server.js
@@ -37,13 +37,9 @@ const app = express();
 // Security & Logging Middleware
 app.use(helmet());
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
-  })
-);
-
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://shopzen-pi.vercel.app']
+}))
 app.use(
   morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'combined')
 );
